23861418d877fdd97990f35645e845e44a7eb9ec
Erin Catto <erincatto@users.noreply.github.com>
2026-07-20 22:05:11 -0700

    Fixes 07 (#97)
    #84 
    #82 
    Added B3_RESTITUTION_ITERATIONS to override restitution iterations.
    Fix regression in hull stacking stability.

--- samples/sample_issues.cpp
+++ samples/sample_issues.cpp
@@ -1146,3 +1146,160 @@ public:
 };
 
 static int sampleWheelStack = RegisterSample( "Issues", "GMod Wheel Stack", WheelStack::Create );
+
+class RestitutionOvershoot : public Sample
+{
+public:
+	static constexpr float m_boxHalf = 0.5f;
+	static constexpr float m_floorHalfXZ = 0.375f;
+	static constexpr float m_floorHalfY = 0.25f;
+	static constexpr float m_dropHeight = 10.0f;
+	static constexpr float m_tolerance = 0.05f;
+
+	explicit RestitutionOvershoot( SampleContext* context )
+		: Sample( context )
+	{
+		if ( context->restart == false )
+		{
+			m_camera->SetView( 20.0f, 0.0f, 28.0f, { 0.0f, m_dropHeight + m_boxHalf, 0.0f } );
+		}
+
+		b3BoxHull floor = b3MakeBoxHull( m_floorHalfXZ, m_floorHalfY, m_floorHalfXZ );
+		b3BodyDef floorDef = b3DefaultBodyDef();
+		floorDef.type = b3_staticBody;
+		floorDef.position = { 0.0f, -m_floorHalfY, 0.0f };
+		b3BodyId floorBody = b3CreateBody( m_worldId, &floorDef );
+
+		b3ShapeDef floorShape = b3DefaultShapeDef();
+		b3CreateHullShape( floorBody, &floorShape, &floor.base );
+
+		b3BoxHull box = b3MakeBoxHull( m_boxHalf, m_boxHalf, m_boxHalf );
+		b3BodyDef boxDef = b3DefaultBodyDef();
+		boxDef.type = b3_dynamicBody;
+		boxDef.position = { 0.0f, m_dropHeight, 0.0f };
+		m_boxBody = b3CreateBody( m_worldId, &boxDef );
+
+		b3ShapeDef boxShape = b3DefaultShapeDef();
+		boxShape.baseMaterial.restitution = 1.0f;
+		b3CreateHullShape( m_boxBody, &boxShape, &box.base );
+
+		m_currentY = m_dropHeight;
+		m_maxBounceY = 0.0f;
+		m_bounced = false;
+		m_failed = false;
+	}
+
+	void Step() override
+	{
+		Sample::Step();
+
+		if ( B3_IS_NON_NULL( m_boxBody ) == false )
+		{
+			return;
+		}
+
+		b3Pos position = b3Body_GetPosition( m_boxBody );
+		m_currentY = position.y;
+
+		b3Vec3 velocity = b3Body_GetLinearVelocity( m_boxBody );
+		if ( m_bounced == false && velocity.y > 0.0f )
+		{
+			m_bounced = true;
+		}
+
+		if ( m_bounced )
+		{
+			if ( position.y > m_maxBounceY )
+			{
+				m_maxBounceY = position.y;
+			}
+			if ( position.y > m_dropHeight + m_tolerance )
+			{
+				m_failed = true;
+			}
+		}
+
+		b3Pos markerPoint = { 0.0f, m_dropHeight + m_boxHalf, 0.0f };
+		DrawPlane( b3Vec3_axisY, markerPoint, MakeColor( b3_colorYellow ) );
+
+		DrawTextLine( "drop height = %.2f m", m_dropHeight );
+		DrawTextLine( "current y   = %.2f m", m_currentY );
+		DrawTextLine( "max bounce  = %.2f m", m_maxBounceY );
+
+		if ( m_bounced == false )
+		{
+			DrawTextLine( "waiting for first bounce..." );
+		}
+		else if ( m_failed )
+		{
+			DrawTextLine( "FAIL: box exceeded drop height" );
+		}
+		else
+		{
+			DrawTextLine( "PASS: bounce stays at or below drop height" );
+		}
+	}
+
+	static Sample* Create( SampleContext* context )
+	{
+		return new RestitutionOvershoot( context );
+	}
+
+	b3BodyId m_boxBody = {};
+	float m_currentY = 0.0f;
+	float m_maxBounceY = 0.0f;
+	bool m_bounced = false;
+	bool m_failed = false;
+};
+
+static int sampleRestitutionOvershoot = RegisterSample( "Issues", "Restitution Overshoot", RestitutionOvershoot::Create );
+
+class SlideTwistOffCenterShape : public Sample
+{
+public:
+	explicit SlideTwistOffCenterShape( SampleContext* context )
+		: Sample( context )
+	{
+		if ( context->restart == false )
+		{
+			m_camera->SetView( -30.0f, 17.0f, 30.0f, { 0.0f, 5.0f, 0.0f } );
+		}
+
+		AddGroundBox( 50.0f );
+
+		b3Quat orientation = b3MakeQuatFromAxisAngle( b3Vec3_axisX, 20.0f * B3_DEG_TO_RAD );
+
+		b3BodyDef bodyDef = b3DefaultBodyDef();
+		b3ShapeDef shapeDef = b3DefaultShapeDef();
+
+		bodyDef.position = { 0.0f, 4.0f, 0.0f };
+		bodyDef.rotation = orientation;
+		b3BodyId planeBody = b3CreateBody( m_worldId, &bodyDef );
+
+		b3BoxHull plane = b3MakeBoxHull( 10.0f, 0.5f, 10.0f );
+		shapeDef.baseMaterial.friction = 0.6f;
+		b3CreateHullShape( planeBody, &shapeDef, &plane.base );
+
+		b3Vec3 boxLocalCenter = { 1.0f, 0.5f, 1.0f };
+		b3Vec3 boxOffset = b3RotateVector( orientation, boxLocalCenter );
+
+		bodyDef.type = b3_dynamicBody;
+		bodyDef.position = { -boxOffset.x, 5.0f - boxOffset.y, -boxOffset.z };
+		bodyDef.rotation = orientation;
+		//bodyDef.angularVelocity = 25.0f * b3RotateVector( orientation, b3Vec3_axisY );
+		b3BodyId boxBody = b3CreateBody( m_worldId, &bodyDef );
+		b3BoxHull mBox = b3MakeOffsetBoxHull( 1.0f, 0.5f, 1.0f, boxLocalCenter );
+		shapeDef.baseMaterial.friction = 0.3f;
+		b3CreateHullShape( boxBody, &shapeDef, &mBox.base );
+
+		b3Body_SetAngularVelocity( boxBody, 25.0f * b3RotateVector( orientation, b3Vec3_axisY ) );
+	}
+
+	static Sample* Create( SampleContext* context )
+	{
+		return new SlideTwistOffCenterShape( context );
+	}
+};
+
+static int sampleSlideTwistOffCenterShape =
+	RegisterSample( "Issues", "Slide Twist Off Center Shape", SlideTwistOffCenterShape::Create );
